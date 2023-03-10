115aee0587ff419304f3d72682cc48e209973928
hasan <hasansennn82@gmail.com>
2023-03-10 15:16:00 +0300

    10.03 V3

--- src/test/java/pages/ieEnstitutu/QalityPage.java
+++
@@ -1,4 +0,0 @@
-package pages.ieEnstitutu;
-
-public class QalityPage {
-}
--- src/test/java/tests/dataProvider.java
+++ src/test/java/tests/dataProvider.java
@@ -2,20 +2,36 @@ package tests;
 
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import pages.ieEnstitutu.QualityPage;
 import utilities.ConfigReader;
 import utilities.Driver;
 
 public class dataProvider {
+    QualityPage qualityPage=new QualityPage();
+
+
     @DataProvider
-    public static  Object[][] gonderilecekMail(){
-
-        Object [][] gonderilecekMailvePasword = {{"nevzat","1234"}};
-        return gonderilecekMailvePasword;
+    public static Object[][] gonderilecekMail(){
+        Object[][] gonderilecekMailvePassword={
+                {"nevzat","1234"},
+                {"nevzat@hotmail.com","123456"},
+                {"ahmet@hotmail.com","nevzat151515151515"},
+                {"celik@hotmail.com","nevzat151515151515"},
+                {"anevzatcelik@gmail.com","Nevzat152032"}};
+        return gonderilecekMailvePassword;
     }
-    @Test(dataProvider = "gonderilecekMail" )
-    public void qualityTest(String mail,String password){
-        Driver.getDriver().get(ConfigReader.getProperty("qdUrl"));
 
+    @Test(dataProvider = "gonderilecekMail")
+    public void qualityTest(String mail,String password){
+        Driver.getDriver().get(ConfigReader.getProperty("url"));
+        qualityPage.providerMethod(mail,password);
+
+    }
+
+    @Test
+    public void dersEklemeTesti(){
+        Driver.getDriver().get(ConfigReader.getProperty("url"));
+        qualityPage.loginMethod();
     }
 
 }
